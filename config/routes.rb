5eff115432f0048a358705f511dc018431862399
gitknkn <atio.asu@gmail.com>
2020-07-17 03:44:58 +0000

    add いいね、コメント、フォロー

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
 
+  # get 'relationships/create'
+  # get 'relationships/destroy'
   root 'home#top'
   get 'home/about'
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member #フォロー一覧作成用
+    get :followers, on: :member #フォロワー一覧作成用
+  end
+  
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
 end
